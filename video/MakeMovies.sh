0c14ddde93a03d546e23e1a7fcacd4266fe7a6a9
uzadow <uzadow@libavg.de>
2007-10-28 10:20:57 +0000

    Removed outdated stuff.
    svn path=/trunk/avg_media/mtc/c-info/; revision=2362

--- video/MakeMovies.sh
+++ video/MakeMovies.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 export IN_MOVIE_DIR=../material/clamp_anim
 
-ffmpeg -i ${IN_MOVIE_DIR}/clamp_64px_%04d.png -intra -vcodec mpeg2video -qscale 4 -an -y clamp_64.mpg
+ffmpeg -i ${IN_MOVIE_DIR}/clamp_64px_%04d.png -vcodec qtrle -y clamp_64.mov
 
